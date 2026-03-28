c5aac10440fdb8deb39091a74d4e2dda628fa758
Hasaan Hameed <hasaanhameed52@gmail.com>
2026-03-28 13:02:31 +0500

    Docker CI/CD implemented

--- botanique_frontend/test/widget_test.dart
+++ botanique_frontend/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:botanique_frontend/main.dart';
+import 'package:botanique/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
